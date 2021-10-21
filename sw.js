5909380d355ce84e76f827e083507402bcd08bb7
Emiel Beeksma <emiel.beeksma@gmail.com>
2021-10-21 11:10:51 +0200

    added install event handler

--- sw.js
+++ sw.js
@@ -1,3 +1,8 @@
 self.addEventListener('activate', () => {
     console.log('Service worker is actief!');
 });
+
+self.addEventListener('install', (ev) => {
+    self.skipWaiting();
+    console.log('Service worker is geïnstalleerd!')
+});
