cfb22312e8c34b293b46c01069874bb767d2c2e9
Emiel Beeksma <emiel.beeksma@gmail.com>
2021-10-21 11:33:45 +0200

    load image and index to cache

--- sw.js
+++ sw.js
@@ -1,8 +1,16 @@
 self.addEventListener('activate', () => {
-    console.log('Service worker is actief!');
+    console.log('Service worker is active!');
 });
 
-self.addEventListener('install', (ev) => {
-    self.skipWaiting();
-    console.log('Service worker is geïnstalleerd!')
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open('v1').then((cache) => {
+            return cache.addAll([
+                './images/',
+                './images/xxxhdpi.png',
+                './index.html',
+            ]);
+        }).then(() => console.log('Cache set up!')),
+    );
+    console.log('Service worker has been installed!')
 });
