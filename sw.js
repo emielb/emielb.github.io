206edc9f1a58c64aca4603499fd17ba12eb4874b
Emiel Beeksma <emiel.beeksma@gmail.com>
2021-10-21 12:13:33 +0200

    added cache clean up

--- sw.js
+++ sw.js
@@ -1,10 +1,25 @@
-self.addEventListener('activate', () => {
+const currentCacheVersion = 'v1';
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((keyList) => {
+            let cleanUp = false;
+            return Promise.all(keyList.map((key) => {
+                if (key !== currentCacheVersion) {
+                    cleanUp = true
+                    return caches.delete(key);
+                }
+            }))
+                .then(() => cleanUp && console.log('Old caches deleted!'))
+                .catch((err) => console.error('Deleting old caches failed:', err));
+        }),
+    );
     console.log('Service worker is active!');
 });
 
 self.addEventListener('install', (event) => {
     event.waitUntil(
-        caches.open('v1').then((cache) => {
+        caches.open(currentCacheVersion).then((cache) => {
             return cache.addAll([
                 './images/xxxhdpi.png',
                 './',
@@ -21,7 +36,7 @@ self.addEventListener('fetch', (event) => {
     event.respondWith(
         caches.match(event.request).then((cachedResp) => {
             return cachedResp || fetch(event.request).then((response) => {
-                return caches.open('v1').then((cache) => {
+                return caches.open(currentCacheVersion).then((cache) => {
                     cache.put(event.request, response.clone());
                     return response;
                 });
